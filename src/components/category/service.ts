11afe262b27df877a0907a969e2c4e85b032929d
Aleksa <milenkovicaleksa94@yahoo.com>
2021-05-16 20:48:30 +0200

    Create a base service class with usable methods

--- src/components/category/service.ts
+++ src/components/category/service.ts
@@ -1,22 +1,18 @@
 import CategoryModel from './model';
-import * as mysql2 from 'mysql2/promise'
 import IModelAdapterOptions from '../../common/IModelAdapterOptions.interface';
 import IErrorResponse from '../../common/IErrorResponse.interface';
 import { IAddCategory } from './dto/IAddCategory';
+import BaseService from '../../services/BaseService';
 
-class CategoryService {
-  private db: mysql2.Connection
+class CategoryService extends BaseService<CategoryModel>{
 
-  constructor(db: mysql2.Connection) {
-    this.db = db;
-  }
-
-  async adaptToModel(row: any, options: Partial<IModelAdapterOptions> = {loadParent: false, loadChildren: false}): Promise<CategoryModel>{
+  protected async adaptModel(row: any, options: Partial<IModelAdapterOptions> = {loadParent: false, loadChildren: false})
+    : Promise<CategoryModel> {
     const item: CategoryModel = new CategoryModel();
 
-    item.categoryId = +(row?.categoryId);
+    item.categoryId = +(row?.category_id);
     item.name = row?.name;
-    item.parentCategoryId = +(row?.parent_category_id);
+    item.parentCategoryId = row?.parent_category_id;
 
     if (options.loadParent && item.parentCategoryId !== null) {
       const data = await this.getById(item.parentCategoryId);
@@ -38,94 +34,32 @@ class CategoryService {
   }
 
   public async getAll(): Promise<CategoryModel[]|IErrorResponse> {
-    return new Promise<CategoryModel[]|IErrorResponse>(async (resolve) => {
-
-      const sql: string = "SELECT * FROM category WHERE parent_category_id IS NULL ORDER BY name;";
-      this.db.execute(sql)
-        .then(async result => {
-          const rows = result[0]
-          console.log(rows)
-          const categories: CategoryModel[] = [];
-          if (Array.isArray(rows)) {
-            for (const row of rows) {
-              categories.push(await this.adaptToModel(row, {loadChildren: true}))
-            }
-          }
-
-          resolve(categories);
-        })
-        .catch(error => {
-          resolve({
-            errorCode: error?.errno,
-            errorMessage: error?.sqlMessage
-          })
-        });
-
-
-    })
+    return await this.getAllByFieldNameFromTable(
+      'category',
+      'parent_category_id',
+      null,
+      {
+        loadChildren: true
+      })
   }
 
   public async getAllByParentCategoryId(parentCategoryId: number): Promise<CategoryModel[]|IErrorResponse> {
-    try {
-      const categories: CategoryModel[] = [];
-
-      const sql: string = "SELECT * FROM category WHERE parent_category_id = ? ORDER BY name;";
-      const [rows, columns] = await this.db.execute(sql, [parentCategoryId]);
-
-      if (Array.isArray(rows)) {
-        for (const row of rows) {
-          categories.push(
-            await this.adaptToModel(
-              row, {
-                loadChildren: true
-              }
-            )
-          )
-        }
-      }
-
-      return categories;
-    } catch (e) {
-      return {
-        errorCode: e?.errno,
-        errorMessage: e?.sqlMessage
-      }
-    }
+    return await this.getAllByFieldNameFromTable(
+      'category',
+      'parent_category_id',
+      parentCategoryId,
+      {
+        loadChildren: true
+      })
   }
 
   public async getById(categoryId: number): Promise<CategoryModel|null|IErrorResponse> {
-    return new Promise<CategoryModel|null|IErrorResponse>(async resolve => {
-      const sql = 'SELECT * FROM category WHERE category_id = ?;'
-      this.db.execute(sql, [categoryId])
-        .then(async result => {
-          const [rows, columns] = result
-
-          if (!Array.isArray(rows)) {
-            resolve(null)
-            return;
-          }
-
-          if (rows.length == 0) {
-            resolve( null);
-            return;
-          }
-
-          resolve(await this.adaptToModel(rows[0], {loadChildren: true, loadParent: true}))
-        })
-        .catch(error => {
-          resolve({
-            errorCode: error?.errno,
-            errorMessage: error?.sqlMessage
-          })
-        })
-
-
-    });
+    return await this.getByIdFromTable('category', categoryId);
   }
 
-  public async add(data: IAddCategory): Promise<CategoryModel | IErrorResponse> {
+  public async add(data: IAddCategory): Promise<CategoryModel|IErrorResponse> {
     return new Promise<CategoryModel|IErrorResponse>(async resolve => {
-      const sql = 'INSERT category SET name = ?, parent_category_id=?;';
+      const sql = 'INSERT category SET name = ?, parent_category_id = ?;';
 
       this.db.execute(sql, [data.name, data.parentCategoryId ?? null])
         .then(async result => {
