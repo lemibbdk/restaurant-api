db86ea0281f366de2729c3e87c40b480f637342c
Aleksa <milenkovicaleksa94@yahoo.com>
2021-09-14 02:28:13 +0200

    Empty db

--- resources/restaurant-empty.sql
+++ resources/restaurant-empty.sql
@@ -12,25 +12,172 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- Data exporting was unselected.
+-- Dumping structure for table restaurant.administrator
+DROP TABLE IF EXISTS `administrator`;
+CREATE TABLE IF NOT EXISTS `administrator` (
+  `administrator_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(30) NOT NULL DEFAULT '0',
+  `password_hash` varchar(255) NOT NULL DEFAULT '0',
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  PRIMARY KEY (`administrator_id`),
+  UNIQUE KEY `uq_administrator_username` (`username`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table restaurant.cart
+DROP TABLE IF EXISTS `cart`;
+CREATE TABLE IF NOT EXISTS `cart` (
+  `cart_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`cart_id`),
+  KEY `fk_cart_user_id` (`user_id`),
+  CONSTRAINT `fk_cart_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table restaurant.cart_item
+DROP TABLE IF EXISTS `cart_item`;
+CREATE TABLE IF NOT EXISTS `cart_item` (
+  `cart_item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cart_id` int(10) unsigned NOT NULL,
+  `item_info_id` int(10) unsigned NOT NULL,
+  `quantity` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`cart_item_id`),
+  UNIQUE KEY `uq_cart_item_cart_id_item_info_id` (`cart_id`,`item_info_id`) USING BTREE,
+  KEY `fk_cart_item_item_info_id` (`item_info_id`),
+  CONSTRAINT `fk_cart_item_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_cart_item_item_info_id` FOREIGN KEY (`item_info_id`) REFERENCES `item_info` (`item_info_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table restaurant.category
+DROP TABLE IF EXISTS `category`;
+CREATE TABLE IF NOT EXISTS `category` (
+  `category_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `parent_category_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`category_id`),
+  UNIQUE KEY `uq_category_name` (`name`),
+  KEY `fk_category_parent_category_id` (`parent_category_id`),
+  CONSTRAINT `fk_category_parent_category_id` FOREIGN KEY (`parent_category_id`) REFERENCES `category` (`category_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=46 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
--- Data exporting was unselected.
+-- Dumping structure for table restaurant.evaluation
+DROP TABLE IF EXISTS `evaluation`;
+CREATE TABLE IF NOT EXISTS `evaluation` (
+  `evaluation_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `order_id` int(10) unsigned NOT NULL,
+  `user_id` int(10) unsigned NOT NULL,
+  `score` enum('1','2','3','4','5') NOT NULL,
+  `remark` varchar(100) NOT NULL,
+  PRIMARY KEY (`evaluation_id`),
+  UNIQUE KEY `uq_evaluation_order_id` (`order_id`),
+  KEY `fk_evaluation_user_id` (`user_id`),
+  CONSTRAINT `fk_evaluation_order_id` FOREIGN KEY (`order_id`) REFERENCES `order` (`order_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_evaluation_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
+
+-- Dumping structure for table restaurant.item
+DROP TABLE IF EXISTS `item`;
+CREATE TABLE IF NOT EXISTS `item` (
+  `item_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `category_id` int(10) unsigned NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `ingredients` varchar(100) NOT NULL,
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  PRIMARY KEY (`item_id`),
+  UNIQUE KEY `uq_item_name` (`name`),
+  KEY `fk_item_category_id` (`category_id`),
+  CONSTRAINT `fk_item_category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=51 DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table restaurant.item_info
+DROP TABLE IF EXISTS `item_info`;
+CREATE TABLE IF NOT EXISTS `item_info` (
+  `item_info_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `item_id` int(10) unsigned NOT NULL,
+  `size` enum('S','L','XL') NOT NULL,
+  `energy_value` decimal(10,2) unsigned NOT NULL DEFAULT 0.00,
+  `mass` decimal(10,2) unsigned NOT NULL DEFAULT 0.00,
+  `price` decimal(10,2) unsigned NOT NULL,
+  PRIMARY KEY (`item_info_id`) USING BTREE,
+  KEY `fk_item_info_item_id` (`item_id`),
+  CONSTRAINT `fk_item_info_item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=131 DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table restaurant.order
+DROP TABLE IF EXISTS `order`;
+CREATE TABLE IF NOT EXISTS `order` (
+  `order_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `cart_id` int(10) unsigned NOT NULL,
+  `status` enum('pending','rejected','accepted','completed') NOT NULL DEFAULT 'pending',
+  `postal_address_id` int(10) unsigned NOT NULL,
+  `desired_delivery_time` time NOT NULL DEFAULT curtime(2),
+  `footnote` text NOT NULL DEFAULT '',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`order_id`),
+  UNIQUE KEY `uq_order_cart_id` (`cart_id`),
+  KEY `fk_order_postal_address_id` (`postal_address_id`),
+  CONSTRAINT `fk_order_cart_id` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`cart_id`) ON UPDATE CASCADE,
+  CONSTRAINT `fk_order_postal_address_id` FOREIGN KEY (`postal_address_id`) REFERENCES `postal_address` (`postal_address_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=27 DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table restaurant.photo
+DROP TABLE IF EXISTS `photo`;
+CREATE TABLE IF NOT EXISTS `photo` (
+  `photo_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `item_id` int(10) unsigned NOT NULL,
+  `image_path` varchar(100) NOT NULL,
+  PRIMARY KEY (`photo_id`),
+  KEY `fk_photo_item_id` (`item_id`),
+  CONSTRAINT `fk_photo_item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=54 DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table restaurant.postal_address
+DROP TABLE IF EXISTS `postal_address`;
+CREATE TABLE IF NOT EXISTS `postal_address` (
+  `postal_address_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `address` text NOT NULL,
+  `phone_number` varchar(24) DEFAULT NULL,
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  PRIMARY KEY (`postal_address_id`) USING BTREE,
+  KEY `fk_postal_address_user_id` (`user_id`),
+  CONSTRAINT `fk_postal_address_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=24 DEFAULT CHARSET=utf8mb4;
+
+-- Data exporting was unselected.
+
+-- Dumping structure for table restaurant.user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `email` varchar(30) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `password_reset_code` varchar(255) DEFAULT NULL,
+  `forename` varchar(64) NOT NULL,
+  `surname` varchar(64) NOT NULL,
+  `is_active` tinyint(1) unsigned NOT NULL DEFAULT 1,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `uq_user_email` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=utf8mb4;
 
 -- Data exporting was unselected.
 
